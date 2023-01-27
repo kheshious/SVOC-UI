d1265f5c90445179c53909fc50af292dc55302b4
Kheshious <kheshious@gmail.com>
2023-01-27 13:33:43 +0200

    sign in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 
 const routes: Routes = [
   
   {path: 'sign-in', component: SignInComponent},
+  {path:'navigation', component: NavigationBarComponent},
   {path : '',pathMatch:'full',component: SignInComponent},
 ];
 
